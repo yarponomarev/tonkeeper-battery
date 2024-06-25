941ea7f058b22f3d1d39b4123d9c73a101bc288b
Ярослав Пономарёв <yaroslavponomarev.pro@gmail.com>
2024-06-25 16:18:28 +0400

    Main блок и блок со скачиванием расширения

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
+/* Проверка поддержки webp */
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+/* Плавная прокрутка */
+import * as mySmoothLinks from "./modules/functions.js";
+
+mySmoothLinks.SmoothLinks();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,6 +1,5 @@
-/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+/* Проверка поддержки webp */
 export function isWebp() {
-    // Проверка поддержки webp
     function testWebP(callback) {
         let webP = new Image();
         webP.onload = webP.onerror = function () {
@@ -8,9 +7,24 @@ export function isWebp() {
         };
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA"
     }
-    // Добавление класса _webp или _no-webp для HTML
     testWebP(function (support) {
         let className = support === true ? 'webp' : 'no-webp';
         document.documentElement.classList.add(className);
     });
+}
+/* Плавная прокрутка */
+export function SmoothLinks() {
+
+    const e = document.querySelectorAll('a[href^="#"]');
+
+    for (let t of e) {
+        t.addEventListener("click", (function(e) {
+            e.preventDefault();
+            const o = t.getAttribute("href");
+            document.querySelector(o).scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            })
+        }))
+    }
 }
