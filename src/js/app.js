514d9956005709788727473ab876b5dc2b920ac1
Ярослав Пономарёв <webdev-yaroslav@list.ru>
2024-06-30 02:16:21 +0400

    Delete src directory

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-/* Проверка поддержки webp */
-import * as flsFunctions from "./modules/functions.js";
-
-flsFunctions.isWebp();
-
-/* Плавная прокрутка */
-import * as myModal from "./modules/functions.js";
-
-myModal.Modal();
