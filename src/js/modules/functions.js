c30499251913f188728dacd26a1b3c057ffbe6fc
Ярослав Пономарёв <yaroslavponomarev.pro@gmail.com>
2024-06-29 18:34:38 +0400

    update

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -37,7 +37,7 @@ export function Modal() {
             ];
             this.events();
         }
-    
+
         events() {
             if (this.modal) {
                 document.addEventListener('click', function(e){
@@ -52,27 +52,27 @@ export function Modal() {
                         this.open();
                         return;
                     }
-    
+
                     if (e.target.closest('.modal-close')) {
                         this.close();
                         return;
                     }
                 }.bind(this));
-    
+
                 window.addEventListener('keydown', function(e) {
                     if (e.keyCode == 27) {
                         if (this.isOpen) {
                             this.close();
                         }
                     }
-    
+
                     if (e.keyCode == 9 && this.isOpen) {
                         this.focusCatch(e);
                         return;
                     }
-    
+
                 }.bind(this));
-    
+
                 this.modal.addEventListener('click', function(e) {
                     if (!e.target.classList.contains('modal__container') && !e.target.closest('.modal__container') && this.isOpen) {
                         this.close();
@@ -80,17 +80,17 @@ export function Modal() {
                 }.bind(this));
             }
         }
-    
+
         open() {
             this.previousActiveElement = document.activeElement;
-    
+
             this.modal.style.setProperty('--transition-time', `${this.speed / 1000}s`);
             this.modal.classList.add('is-open');
             this.disableScroll();
             
             this.modalContainer.classList.add('modal-open');
             this.modalContainer.classList.add(this.animation);
-    
+
             setTimeout(() => {
                 this.options.isOpen(this);
                 this.modalContainer.classList.add('animate-open');
@@ -98,37 +98,37 @@ export function Modal() {
                 this.focusTrap();
             }, this.speed);
         }
-    
+
         close() {
             if (this.modalContainer) {
                 this.modalContainer.classList.remove('animate-open');
                 this.modalContainer.classList.remove(this.animation);
                 this.modal.classList.remove('is-open');
                 this.modalContainer.classList.remove('modal-open');
-    
+
                 this.enableScroll();
                 this.options.isClose(this);
                 this.isOpen = false;
                 this.focusTrap();
             }
         }
-    
+
         focusCatch(e) {
             const focusable = this.modalContainer.querySelectorAll(this.focusElements);
             const focusArray = Array.prototype.slice.call(focusable);
             const focusedIndex = focusArray.indexOf(document.activeElement);
-    
+
             if (e.shiftKey && focusedIndex === 0) {
                 focusArray[focusArray.length - 1].focus();
                 e.preventDefault();
             }
-    
+
             if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
                 focusArray[0].focus();
                 e.preventDefault();
             }
         }
-    
+
         focusTrap() {
             const focusable = this.modalContainer.querySelectorAll(this.focusElements);
             if (this.isOpen) {
@@ -137,7 +137,7 @@ export function Modal() {
                 this.previousActiveElement.focus();
             }
         }
-    
+
         disableScroll() {
             let pagePosition = window.scrollY;
             this.lockPadding();
@@ -145,7 +145,7 @@ export function Modal() {
             document.body.dataset.position = pagePosition;
             document.body.style.top = -pagePosition + 'px';
         }
-    
+
         enableScroll() {
             let pagePosition = parseInt(document.body.dataset.position, 10);
             this.unlockPadding();
@@ -154,7 +154,7 @@ export function Modal() {
             window.scroll({ top: pagePosition, left: 0 });
             document.body.removeAttribute('data-position');
         }
-    
+
         lockPadding() {
             let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
             this.fixBlocks.forEach((el) => {
@@ -162,7 +162,7 @@ export function Modal() {
             });
             document.body.style.paddingRight = paddingOffset;
         }
-    
+
         unlockPadding() {
             this.fixBlocks.forEach((el) => {
                 el.style.paddingRight = '0px';
@@ -170,7 +170,7 @@ export function Modal() {
             document.body.style.paddingRight = '0px';
         }
     }
-    
+
     const modal = new Modal({
         isOpen: (modal) => {
             console.log(modal);
